df236a47c2327e63e1a10df7f3d2a225dfe4bf29
SehrishZarin <sehrishzarin45@gmail.com>
2025-06-16 21:05:12 +0500

    project 1 complete

--- project-1/src/components/accordian/index.jsx
+++ project-1/src/components/accordian/index.jsx
@@ -1,32 +1,57 @@
 import { useState } from "react";
 import data from "./data";
+import "./styles.css";
 
 export default function Accordion() {
     const [selected, setSelected] = useState(null);
-    function handleSingleSelection(curId){
-       setSelected(curId)
+    const [enableMultiple, setEnableMultiple] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+    function handleSingleSelection(curId) {
+        setSelected(curId === selected ? null : curId);
     }
+
+    function handleMultiSelection(curId) {
+        let cpyMultiple = [...multiple];
+        const findIndex = cpyMultiple.indexOf(curId);
+        if (findIndex !== -1) {
+            cpyMultiple.splice(findIndex, 1); 
+        } else {
+            cpyMultiple.push(curId); 
+        }
+        setMultiple(cpyMultiple);
+    }
+
     return (
         <div className="wrapper">
+
             <div className="accordion">
-                {
-                    data && data.length > 0 ? (
-                        data.map((dataitem, index) => (
-                            <div className="item" key={index}>
-                                <div onClick={()=>handleSingleSelection(dataitem.id)} className="title">
-                                    <h3>{dataitem.question}</h3>
-                                    <span>+</span>
-                                    {
-                                        selected === dataitem.id ? <div>{dataitem.answer}</div> : null
-                                    }
-                                </div>
-                               
+            <button onClick={() => setEnableMultiple(!enableMultiple)}>
+                {enableMultiple ? "Switch to Single Select" : "Switch to Multi Select"}
+            </button>
+                {data && data.length > 0 ? (
+                    data.map((dataitem, index) => (
+                        <div className="item" key={index}>
+                            <div
+                                onClick={() =>
+                                    enableMultiple
+                                        ? handleMultiSelection(dataitem.id)
+                                        : handleSingleSelection(dataitem.id)
+                                }
+                                className="title"
+                            >
+                                <h3>{dataitem.question}</h3>
+                                <span>+</span>
                             </div>
-                        ))
-                    ) : (
-                        <div>No data found</div>
-                    )
-                }
+
+                            {enableMultiple
+                                ? multiple.includes(dataitem.id) && <div>{dataitem.answer}</div>
+                                : selected === dataitem.id && <div>{dataitem.answer}</div>}
+                        </div>
+                    ))
+                ) : (
+                    <div>No data found</div>
+                )}
             </div>
         </div>
     );
