3500f23d533e720e7fcbfcf05f75ffa7bd3aa81e
SehrishZarin <sehrishzarin45@gmail.com>
2025-06-18 12:52:04 +0500

    second project complete

---
+++ project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+// import Accordion from './components/project 1/accordian'
+import RandomColor from './components/project 2/randomcolor'
+
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      {/* <Accordion/> */}
+      <RandomColor/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
