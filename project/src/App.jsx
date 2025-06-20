c80844daeb6170528d7c1a2db6206128209021d1
SehrishZarin <sehrishzarin45@gmail.com>
2025-06-20 16:00:52 +0500

    project 4 done

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import StarRating from './components/project 3/rating'
+// import StarRating from './components/project 3/rating'
 // import Accordion from './components/project 1/accordian'
 // import RandomColor from './components/project 2/randomcolor'
-
+import ImageSlider from './components/project 4/image-slider'
 
 function App() {
 
@@ -11,7 +11,13 @@ function App() {
     <div className="App">
       {/* <Accordion/> */}
       {/* <RandomColor/> */}
-      <StarRating/>
+      {/* <StarRating/> */}
+          {/* Image slider component */}
+       <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      /> 
     </div>
       
     </>
