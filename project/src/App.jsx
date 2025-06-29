6eb25fad6032eb5f3856edb66b45ef9fbccb21b0
SehrishZarin <sehrishzarin45@gmail.com>
2025-06-30 02:25:22 +0500

    project 7 + 8

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import QRCodeGenerator from './components/project 7/qr-code'
+import LightDarkMode from './components/project 8/themes'
 // import StarRating from './components/project 3/rating'
 // import Accordion from './components/project 1/accordian'
 // import RandomColor from './components/project 2/randomcolor'
-import ImageSlider from './components/project 4/image-slider'
-import LoadMoreData from './components/project 5/load-more-data'
+// import ImageSlider from './components/project 4/image-slider'
+// import LoadMoreData from './components/project 5/load-more-data'
 
 function App() {
 
@@ -19,7 +21,9 @@ function App() {
         page={"1"}
         limit={"10"}
       />  */}
-      <LoadMoreData/>
+      {/* <LoadMoreData/> */}
+      {/* <QRCodeGenerator/> */}
+      <LightDarkMode/>
     </div>
       
     </>
